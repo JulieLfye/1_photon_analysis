af974730e2ee933c41165eaba091b166e9882759
Julie <julie.lafaye07@gmail.com>
2021-03-03 16:03:43 +0100

    with avi movie, works

--- extract_tail_movement.m
+++ extract_tail_movement.m
@@ -16,8 +16,8 @@ clear;
 close all;
 clc;
 
-% root = '/home/julie/Sciences/Danionella';
-root = '/media/ljp/Julie_Data/Data/Danionella/Calcium_imaging';
+root = '/home/julie/Sciences/Danionella';
+% root = '/media/ljp/Julie_Data/Data/Danionella/Calcium_imaging';
 % root = '/run/media/julie/Julie_Data/Data/Danionella/Calcium_imaging';
 % root = '/home/julie/Sciences';
 % root = '/home/ljp/Sciences';
@@ -32,21 +32,23 @@ run_txt = ['Run ' num2str(d) num2str(u)];
 
 % name of the movie
 folder_name = 'tail_cropped';
-tifname = 'trial_1_grey.tif';
+avi_name = 'trial_1_grey.avi';
 
 % load movie
 path = fullfile(root,study,date,run_txt,'behavior',folder_name);
 
-tstack = Tiff(fullfile(path,tifname));
-[I,J] = size(tstack.read());
-K = length(imfinfo(fullfile(path,tifname)));
-data = zeros(I,J,K);
-data(:,:,1)  = tstack.read();
-for n = 2:K
-    tstack.nextDirectory()
-    data(:,:,n) = tstack.read();
-     if mod(n,2000) == 0
-        fprintf('opening image %d \n', n)
+v = VideoReader(fullfile(path,avi_name));
+
+%%
+tail_matrix = zeros(v.NumFrames,v.Height);
+
+tic
+for k = 1:v.NumFrames
+    im = read(v,1);
+    m = im(:,:,1);
+    tail_matrix(k,:) = sum(m,2)';
+    if mod(k,2000) == 0
+        fprintf('image %d \n', k)
     end
-    
 end
+toc
